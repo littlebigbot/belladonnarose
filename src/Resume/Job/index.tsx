0fa25bfe4eccda789b1cba17e10508274d0e47b4
Belladonna Jessamine Rose <belladonna.cordelia@gmail.com>
2023-05-07 18:41:13 -0400

    Fleshed resume out

---
+++ src/Resume/Job/index.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import dayjs from 'dayjs';
+
+import styles from './index.module.scss';
+
+type JobProps = {
+  title: string;
+  company: string;
+  start: string;
+  end: string;
+  location: string;
+  details: string[];
+  tags: string[];
+  type: string;
+  url?: string;
+};
+
+const DEFAULT_DATE_FORMAT = 'MMM YYYY';
+
+function Job({
+               title,
+               type,
+               company,
+               start,
+               end,
+               location,
+               details,
+               tags,
+               url,
+             }: JobProps): React.ReactElement {
+
+  // start, end
+
+  const startDate = dayjs(start);
+  const endDate = end === 'current' ? dayjs() : dayjs(end);
+  const timePeriod = endDate.diff(startDate, 'month');
+
+  return <div className={styles.Job}>
+    <h3>
+      <strong>{title}</strong>
+      &nbsp;@&nbsp;
+      { url?.length ? <a href={url} rel="noreferrer" target="_blank">{company}</a> : company }
+      &nbsp;
+      <small>({type})</small>
+    </h3>
+    <h5>
+      {startDate.format(DEFAULT_DATE_FORMAT)}
+      &nbsp;-&nbsp;
+      {end === 'current' ? 'Present' : endDate.format(DEFAULT_DATE_FORMAT)}
+      &nbsp;|&nbsp;
+      {location}
+    </h5>
+    <ul className={styles.detail}>
+      {details.map((detail, index) => <li key={index}>{detail}</li>)}
+    </ul>
+    <ul className={styles.tags}>
+      {tags.map((tag, index) => <li key={index}>{tag}</li>)}
+    </ul>
+  </div>;
+}
+
+export default Job;
--- src/Resume/index.tsx
+++ src/Resume/index.tsx
@@ -1,8 +1,71 @@
 import * as React from 'react';
-import './index.scss';
+
+import styles from  './index.module.scss';
+import jobs from './jobs.json';
+import Job from './Job';
 
 function Resume(): React.ReactElement {
-  return <div className="Resume">Resume</div>;
+  return <div className={styles.Resume}>
+    <h1>Belladonna Rose</h1>
+
+    <ul className={styles.links}>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/belladonna-rose-2321773b/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/littlebigbot"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Business GitHub
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/littlebigbot"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Personal GitHub
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://stackoverflow.com/users/1132889/belladonna"
+          rel="noreferrer"
+          target="_blank"
+        >
+          StackOverflow
+        </a>
+      </li>
+    </ul>
+
+    <div className={styles.summary}>
+      <h3>Background</h3>
+      <p>
+        Hi, my name is Belladonna Rose. I am a full stack developer with a passion for building beautiful, functional, and accessible web applications. I have a background in design and a love for learning new things. I am a self-starter and a team player. I am looking for a position where I can grow and learn new skills.
+      </p>
+      <h3>Education</h3>
+      <p>
+        Bachelor of Arts, Computer Science & Mathematics
+      </p>
+      <p>
+        <strong>Hunter College, New York, NY</strong> (2006-2010)
+      </p>
+    </div>
+
+   <div className={styles.jobs}>
+     { jobs.map((job, index) => <Job key={index} {...job} />) }
+   </div>
+
+  </div>;
 }
 
 export default Resume;
