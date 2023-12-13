49cffa9ea78157ba97db1f1aa14de2b0cb4da17f
Belladonna Jessamine Rose <belladonna.cordelia@gmail.com>
2023-12-13 14:22:22 -0500

    making it a resume

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import * as React from 'react';
 import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
 import Resume from './Resume';
 import './App.scss';
 import HtmlClasses from './HtmlClasses';
@@ -21,12 +19,6 @@ function App(): React.ReactElement {
 
         <Switch>
           <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/resume">
             <Resume />
           </Route>
         </Switch>
